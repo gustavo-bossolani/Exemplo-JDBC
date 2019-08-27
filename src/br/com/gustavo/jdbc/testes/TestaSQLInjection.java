987577ce226b5bd1c19d76cf9c97f636cca1b690
gustavo-bossolani <gustavo.wyd@outlook.com>
2019-08-27 18:09:49 -0300

    teste de sql injection

---
+++ src/br/com/gustavo/jdbc/testes/TestaSQLInjection.java
@@ -0,0 +1,68 @@
+package br.com.gustavo.jdbc.testes;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import br.com.gustavo.jdbc.models.Banco;
+
+public class TestaSQLInjection {
+
+
+	public static void main(String[] args) {
+
+		/*
+		 * Para evitar SQLInjection é recomendado escrever a query em uma String antes 
+		 * de passar para o método Connection.prepareStatement(query) passando os values como '?'
+		 * Connection.prepareStatement() será responsável por tratar os valores que estão entrando
+		 * no banco.
+		 * 
+		 * Depois que o método Connection.prepareStatement() é chamado devemos setar os parâmetros
+		 * com o método PreparedStatement.setString(pos, value) começando com a posição sempre do 1
+		 * e especificando o tipo primitivo dos valores ex: PreparedStatement.SetInt().
+		 * Para excutar a query já tratada, basta apenas usar PreparedStatement.execute() sem parâmetros.
+		 * 
+		 * Connection.prepareStatement(sql, param) este método deve receber o segundo parâmetro antes dos 
+		 * valores serem Setados com PreparedStatement.setString(pos, value).
+		 */
+		
+		String sql = "insert into Produto(nome, descricao) values(?,?)";
+		String nome = "TV HD+ 45'";
+		String descricao = "TV ULTRA HD para assistir filmes em alta definição";
+		PreparedStatement statementPreparado;
+
+		try {
+
+			Connection connection = Banco.getConnection();
+			statementPreparado = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+			statementPreparado.setString(1, nome);
+			statementPreparado.setString(2, descricao);
+			boolean resultado = statementPreparado.execute();
+
+			if (!resultado) {
+				System.out.println("Produto Inserido na base de Dados.");
+				int linhasEditadas = statementPreparado.getUpdateCount();
+				System.out.println("Linhas alteradas " + linhasEditadas);
+				
+				
+			} else {
+				System.out.println("Selecionando Produtos na base de dados.");
+				ResultSet resultSet = statementPreparado.getResultSet();
+				while (resultSet.next()) {
+					System.out.println("id: " + resultSet.getInt("id") + " nome: " + resultSet.getString("nome")
+							+ " descricao: " + resultSet.getString("descricao"));
+				}
+			}
+
+			System.out.println();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.out.println(e.getMessage());
+		}
+
+	}
+
+}
